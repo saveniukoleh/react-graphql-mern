2e7218310112d5b2f85fd3479e37e00a15ca5e9c
Oleh Saveniuk <oleh.saveniuk@gmail.com>
2022-09-14 09:51:25 +0200

    Init

---
+++ server/sampleData.js
@@ -0,0 +1,77 @@
+const projects = [
+  {
+    id: "1",
+    clientId: "1",
+    name: "eCommerce Website",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    status: "In Progress",
+  },
+  {
+    id: "2",
+    clientId: "2",
+    name: "Dating App",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    status: "In Progress",
+  },
+  {
+    id: "3",
+    clientId: "3",
+    name: "SEO Project",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    status: "In Progress",
+  },
+  {
+    id: "4",
+    clientId: "4",
+    name: "Design Prototype",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    status: "In Progress",
+  },
+  {
+    id: "5",
+    clientId: "5",
+    name: "Auction Website",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    status: "In Progress",
+  },
+];
+
+const clients = [
+  {
+    id: "1",
+    name: "Tony Stark",
+    email: "ironman@gmail.com",
+    phone: "343-567-4333",
+  },
+  {
+    id: "2",
+    name: "Natasha Romanova",
+    email: "blackwidow@gmail.com",
+    phone: "223-567-3322",
+  },
+  {
+    id: "3",
+    name: "Thor Odinson",
+    email: "thor@gmail.com",
+    phone: "324-331-4333",
+  },
+  {
+    id: "4",
+    name: "Steve Rugers",
+    email: "steve@gmail.com",
+    phone: "344-562-6787",
+  },
+  {
+    id: "5",
+    name: "Bruce Banner",
+    email: "bruce@gmail.com",
+    phone: "321-468-8887",
+  },
+];
+
+module.exports = { projects, clients };
